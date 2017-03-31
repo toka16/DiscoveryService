dfdb5b196410d4293ed1cc3036f390f62eabc93e
toka16 <tabra12@freeuni.edu.ge>
2017-03-31 19:01:35 +0400

    Fix empty list bug

--- src/main/java/ge/ambro/discoveryservice/repo/InmemoryServiceRepository.java
+++ src/main/java/ge/ambro/discoveryservice/repo/InmemoryServiceRepository.java
@@ -9,6 +9,7 @@ import ge.ambro.discoveryservice.dto.ResolvedTargetResponseDTO;
 import ge.ambro.discoveryservice.dto.ServiceDTO;
 import ge.ambro.discoveryservice.dto.TargetResponseDTO;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -48,7 +49,7 @@ public class InmemoryServiceRepository implements ServiceRepository {
 
     @Override
     public Collection<ServiceDTO> getByName(String name) {
-        return servicesByName.get(name).stream()
+        return servicesByName.getOrDefault(name, Collections.emptyList()).stream()
                 .filter(ServiceDTO::isAlive)
                 .collect(Collectors.toList());
     }
